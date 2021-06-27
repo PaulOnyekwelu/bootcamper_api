1ed1524fa60826c2d9a9ab7ad46d9b38bad01c3d
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-06-27 08:47:32 +0100

    routing based on method and request url using node http module

--- server.js
+++ server.js
@@ -1,17 +1,51 @@
-const http = require('http')
+const http = require("http");
 
 const todos = [
-  {id: 1, title: "learn js"},
-  {id: 2, title: "learn React"},
-  {id: 3, title: "learn Node"},
-]
+  { id: 1, title: "learn js" },
+  { id: 2, title: "learn React" },
+  { id: 3, title: "learn Node" },
+];
 
 const server = http.createServer((req, res) => {
-  if(req.url === '/') {
-    res.setHeader('Content-Type', 'Application/json')
-    res.end(JSON.stringify({todos}))
-  }
-})
+  let body = [];
+  req
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = JSON.parse(Buffer.concat(body).toString());
+
+      res.setHeader("Content-Type", "application/json");
+      res.statusCode = 404;
+      const resp = {
+        success: false,
+        data: null,
+        error: null,
+      };
+      const { method, url } = req;
+
+      if (url === "/todos") {
+        if (method === "POST") {
+          const { id, title } = body;
+          if (!id || !title) {
+            resp.error = "id and title fields are required";
+          } else {
+            todos.push({ id, title });
+            res.statusCode = 201;
+            resp.success = true;
+            resp.data = todos;
+          }
+        }
+        if (method === "GET") {
+          resp.data = todos;
+          resp.success = true;
+          res.statusCode = 200;
+        }
+      }
+
+      res.end(JSON.stringify(resp));
+    });
+});
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
